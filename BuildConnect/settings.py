151837e3d74a28ed0f8d88299a00a3e4dd3fb7d2
feik123 <feik@abv.bg>
2026-04-07 13:42:52 +0300

    fix: database config for render

--- BuildConnect/settings.py
+++ BuildConnect/settings.py
@@ -98,24 +98,11 @@ WSGI_APPLICATION = 'BuildConnect.wsgi.application'
 
 DATABASE_URL = os.getenv('DB_NAME')
 
-if DATABASE_URL:
-
-    DATABASES = {
-        'default': dj_database_url.parse(
-            os.environ.get('DATABASE_URL'),
-            default='postgres://postgres:postgres@localhost:5432/buldconnectdb'
-        )
-    }
-
-else:
-
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://postgres:postgres@localhost:5432/buildconnectdb'
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
